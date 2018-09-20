3668c265015f8bf3420d0f0e41a20af77eeb2388
LuisSas <luis@makingdevs.com>
2018-09-19 19:59:05 -0500

    Put database configuration

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :learn_phoenix, LearnPhoenix.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "luissas",
+  password: "avatar65",
   database: "learn_phoenix_dev",
   hostname: "localhost",
   pool_size: 10
